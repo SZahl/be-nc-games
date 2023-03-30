97ab45a29ff2a22bb64396386eab0bbe908b3bad
SZahl <szahl@hotmail.co.uk>
2023-03-30 18:32:52 +0100

    built post /api/reviews/:review_id/comments endpoint and tests

--- app.controllers.js
+++ app.controllers.js
@@ -1,4 +1,4 @@
-const { fetchCategories, fetchReviewByID, fetchAllReviews, fetchCommentsByReviewID, checkReviewExists } = require('./app.models.js');
+const { fetchCategories, fetchReviewByID, fetchAllReviews, fetchCommentsByReviewID, checkReviewExists, insertComment, checkUserExists } = require('./app.models.js');
 
 exports.getCategories = (request, response) => {
     fetchCategories().then((categories) => {
@@ -36,6 +36,19 @@ exports.getCommentsByReviewID = (request, response, next) => {
     Promise.all(reviewCommentsPromises)
     .then((comments) => {
         response.status(200).send({ comments: comments[1]})
+    })
+    .catch((error) => {
+        next(error);
+    })
+}
+
+exports.postComment = (request, response, next) => {
+    const { review_id } = request.params;
+    const commentUserPromises = [checkReviewExists(review_id), checkUserExists(request.body.username), insertComment(request.body, review_id)];
+
+    Promise.all(commentUserPromises)
+    .then((newComment) => {
+        response.status(201).send({ comment: newComment[2]})
     })
     .catch((error) => {
         next(error);
--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { getCategories, getAllReviews, getReviewByID, getCommentsByReviewID } = require('./app.controllers.js');
+const { getCategories, getAllReviews, getReviewByID, getCommentsByReviewID, postComment } = require('./app.controllers.js');
 const { handleErrorCodes, handleCustomError, handleServerError } = require('./error.controllers.js')
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/api/categories', getCategories);
 
@@ -10,7 +12,9 @@ app.get('/api/reviews', getAllReviews)
 
 app.get('/api/reviews/:review_id', getReviewByID);
 
-app.get('/api/reviews/:review_id/comments', getCommentsByReviewID)
+app.get('/api/reviews/:review_id/comments', getCommentsByReviewID);
+
+app.post('/api/reviews/:review_id/comments', postComment);
 
 app.all('*', (request, response, next) => {
     response.status(404).send({ message: 'Path not found'})
